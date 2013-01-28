92c3129b3f0fe833b32312be7b0f5d8dafc6b297
Vince Fonte <v.fonte91@gmail.com>
2013-01-28 03:45:58 +0000

    Added new versions of jquery to config

--- SOAP/SOAP/App_Start/BundleConfig.cs
+++ SOAP/SOAP/App_Start/BundleConfig.cs
@@ -9,10 +9,10 @@ namespace SOAP
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-1.9.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
-                        "~/Scripts/jquery-ui-{version}.js"));
+                        "~/Scripts/jquery-ui-1.10.custom.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.unobtrusive*",
