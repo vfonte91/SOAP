d998238256fc43315897fd928d1c60bd61be7d79
rotonda.7 <rotonda.7@buckeyemail.osu.edu>
2013-02-06 01:16:34 -0500

    Now application will point to correct location

--- SOAP/SOAP/App_Start/RouteConfig.cs
+++ SOAP/SOAP/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace SOAP
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "SampleFormTabs", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
