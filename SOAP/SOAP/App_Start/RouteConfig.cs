0d1adf39ba336eab301632c409bffef8c8406aec
Vince Fonte <v.fonte91@gmail.com>
2013-03-19 12:32:07 -0400

    Fixed issues with routing

--- SOAP/SOAP/App_Start/RouteConfig.cs
+++ SOAP/SOAP/App_Start/RouteConfig.cs
@@ -11,13 +11,12 @@ namespace SOAP
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
-            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
+
     }
 }
