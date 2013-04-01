8f01659316dc314d2de9113299ff09b0f359ddcb
mtimko <timko.29@buckeyemail.osu.edu>
2013-04-01 18:19:28 -0400

    export testing and adding .dll

--- SOAP/SOAP/Controllers/Service.cs
+++ SOAP/SOAP/Controllers/Service.cs
@@ -721,5 +721,362 @@ namespace SOAP.Controllers
         }
 
         #endregion
+
+        public void Page_Load(object sender, EventArgs e)
+        {
+
+
+            #region SIMPLE TEXT OUTPUT
+            /*
+        string path = Server.MapPath("PDFs");
+        BaseFont bfTimes = BaseFont.CreateFont(BaseFont.TIMES_ROMAN, BaseFont.CP1252, false);
+        Font times = new Font(bfTimes, 9, Font.NORMAL);
+        Document doc = new Document();
+
+        try
+        {
+            doc.Open();
+
+            doc.Add(new Paragraph("This is font size 9, times new roman type", times));
+            
+        }
+        catch
+        {
+
+        }
+        finally
+        {
+            doc.Close();
+        }
+*/
+            #endregion
+
+            #region WORD WRAPPING
+            /*
+        string path = Server.MapPath("PDFs");
+        Document doc = new Document(PageSize.LETTER);
+
+        BaseFont bfTimes = BaseFont.CreateFont(BaseFont.TIMES_ROMAN, BaseFont.CP1252, false);
+        Font times = new Font(bfTimes, 9, Font.NORMAL);
+
+        try
+        {
+            PdfWriter.GetInstance(doc, new FileStream(path + "/WordWrapping.pdf", FileMode.Create));
+            doc.Open();
+            Chunk c1 = new Chunk("THHHHHIS IS A PHHHHHHHRASE?  ", times);
+
+            Phrase phrase = new Phrase();
+            phrase.Add(c1);
+
+            for (int i = 1; i < 400; i++)
+            {
+                doc.Add(phrase);
+            }
+
+         }
+        catch
+        {
+
+        }
+        finally
+        {
+            doc.Close();
+        }
+        doc.Close();
+
+*/
+            #endregion
+
+            #region LISTS
+            /*
+        string path = Server.MapPath("PDFs");
+        Document doc = new Document(PageSize.LETTER);
+
+        try
+        {
+            PdfWriter.GetInstance(doc, new FileStream(path + "/Lists.pdf", FileMode.Create));
+            doc.Open();
+
+            List list = new List(List.UNORDERED);
+
+            list.Add("One");
+            list.Add("Two");
+            list.Add("Three");
+            list.Add("Four");
+            list.Add("Five");
+            Paragraph paragraph = new Paragraph();
+            string text = "Lists";
+            paragraph.Add(text);
+            doc.Add(paragraph);
+            doc.Add(list);
+        }
+        catch (DocumentException dex)
+        {
+            Response.Write(dex.Message);
+        }
+        catch (IOException ioex)
+        {
+            Response.Write(ioex.Message);
+        }
+        finally
+        {
+            doc.Close();
+        }
+*/
+            #endregion
+
+            #region COORDINATE TEXT?
+            /*
+        string pdfpath = Server.MapPath("PDFs");
+        Document doc = new Document(PageSize.LETTER);
+
+        try
+        {
+            PdfWriter writer = PdfWriter.GetInstance(doc, new FileStream(pdfpath + "/Graphics.pdf", FileMode.Create));
+            doc.Open();
+
+            PdfContentByte cb = writer.DirectContent;
+
+            cb.MoveTo(doc.PageSize.Width / 2, doc.PageSize.Height / 2);
+            cb.LineTo(doc.PageSize.Width / 2, doc.PageSize.Height);
+            cb.Stroke();
+            cb.MoveTo(0, doc.PageSize.Height / 2);
+            cb.LineTo(doc.PageSize.Width, doc.PageSize.Height / 2);
+            cb.Stroke();
+
+
+            //top left corner square
+            cb.MoveTo(100f, 700f);
+            cb.LineTo(200f, 700f);
+            cb.LineTo(200f, 600f);
+            cb.LineTo(100f, 600f);
+            cb.ClosePath();
+            cb.Stroke();
+
+            //easier way to make a square in right corner
+            cb.Rectangle(doc.PageSize.Width - 200f, 600f, 100f, 100f);
+            cb.Stroke();
+
+            cb.SetColorStroke(new CMYKColor(1f, 0f, 0f, 0f));
+            cb.SetColorFill(new CMYKColor(0f, 0f, 1f, 0f));
+
+            ////Path closed and stroked square
+            cb.MoveTo(70, 200);
+            cb.LineTo(170, 200);
+            cb.LineTo(170, 300);
+            cb.LineTo(70, 300);
+            cb.ClosePathStroke();
+
+            //Filled, but not stroked or closed square
+            cb.MoveTo(190, 200);
+            cb.LineTo(290, 200);
+            cb.LineTo(290, 300);
+            cb.LineTo(190, 300);
+            cb.Fill();
+
+            //Path closed, stroked and filled square
+            cb.MoveTo(430, 200);
+            cb.LineTo(530, 200);
+            cb.LineTo(530, 300);
+            cb.LineTo(430, 300);
+            cb.ClosePathFillStroke();
+
+        }
+        catch
+        {
+
+        }
+        finally
+        {
+            doc.Close();
+        }
+*/
+            #endregion
+
+            #region IMAGE TO PDF
+            /*
+        string pdfpath = Server.MapPath("PDFs");
+        string imagepath = Server.MapPath("Images");
+        Document doc = new Document();
+        try
+        {
+
+            PdfWriter.GetInstance(doc, new FileStream(pdfpath + "/SOAP_updated.pdf", FileMode.Create));
+            doc.Open();
+
+            BaseFont bfTimes = BaseFont.CreateFont(BaseFont.TIMES_ROMAN, BaseFont.CP1252, false);
+            Font times = new Font(bfTimes, 12, Font.NORMAL);
+            doc.Add(new Paragraph("OSU Anesthesia Vet SOAP Form:", times));
+
+            iTextSharp.text.Image img = iTextSharp.text.Image.GetInstance(imagepath + "/AnesthesiaSoapPicture2.jpg");
+            doc.Add(img);
+        }
+        catch (Exception ex)
+        {
+            //Log error?
+        }
+        finally
+        {
+            doc.Close();
+        }
+
+  */
+            #endregion
+
+            #region PAGE LAYOUT
+            /*
+        string pdfpath = Server.MapPath("PDFs");
+        string imagepath = Server.MapPath("Images");
+        FontFactory.RegisterDirectory("C:\\WINDOWS\\Fonts");
+
+        Document doc = new Document();
+
+        BaseFont bfTimes = BaseFont.CreateFont(BaseFont.TIMES_ROMAN, BaseFont.CP1252, false);
+        iTextSharp.text.Font font1 = new iTextSharp.text.Font(bfTimes, 9, iTextSharp.text.Font.NORMAL);
+        BaseFont bfHal = BaseFont.CreateFont(BaseFont.HELVETICA, BaseFont.CP1252, false);
+        iTextSharp.text.Font font2 = new iTextSharp.text.Font(bfHal, 12, iTextSharp.text.Font.BOLD);
+
+
+        doc.SetMargins(45f, 45f, 60f, 60f);
+        try
+        {
+            FileStream output = new FileStream(pdfpath + "/IrregularColumns.pdf", FileMode.Create);
+            PdfWriter writer = PdfWriter.GetInstance(doc, output);
+            doc.Open();
+            PdfContentByte cb = writer.DirectContent;
+            ColumnText ct = new ColumnText(cb);
+            ct.Alignment = Element.ALIGN_JUSTIFIED;
+
+            Paragraph heading = new Paragraph("Chapter 1", font1);
+            heading.Leading = 40f;
+            doc.Add(heading);
+            iTextSharp.text.Image L = iTextSharp.text.Image.GetInstance(imagepath + "/l.gif");
+            L.SetAbsolutePosition(doc.Left, doc.Top - 180);
+            doc.Add(L);
+
+            ct.AddText(new Phrase("orem ipsum dolor sit amet, consectetuer adipiscing elit. Suspendisse blandit blandit turpis. Nam in lectus ut dolor consectetuer bibendum. Morbi neque ipsum, laoreet id; dignissim et, viverra id, mauris. Nulla mauris elit, consectetuer sit amet, accumsan eget, congue ac, libero. Vivamus suscipit. Nunc dignissim consectetuer lectus. Fusce elit nisi; commodo non, facilisis quis, hendrerit eu, dolor? Suspendisse eleifend nisi ut magna. Phasellus id lectus! Vivamus laoreet enim et dolor. Integer arcu mauris, ultricies vel, porta quis, venenatis at, libero. Donec nibh est, adipiscing et, ullamcorper vitae, placerat at, diam. Integer ac turpis vel ligula rutrum auctor! Morbi egestas erat sit amet diam. Ut ut ipsum? Aliquam non sem. Nulla risus eros, mollis quis, blandit ut; luctus eget, urna. Vestibulum vestibulum dapibus erat. Proin egestas leo a metus?\n\n", font2));
+            ct.AddText(new Phrase("Vivamus enim nisi, mollis in, sodales vel, convallis a, augue? Proin non enim. Nullam elementum euismod erat. Aliquam malesuada eleifend quam! Nulla facilisi. Aenean ut turpis ac est tempor malesuada. Maecenas scelerisque orci sit amet augue laoreet tempus. Duis interdum est ut eros. Fusce dictum dignissim elit. Morbi at dolor. Fusce magna. Nulla tellus turpis, mattis ut, eleifend a, adipiscing vitae, mauris. Pellentesque mattis lobortis mi.\n\n", font2));
+            ct.AddText(new Phrase("Nullam sit amet metus scelerisque diam hendrerit porttitor. Aenean pellentesque, lorem a consectetuer consectetuer, nunc metus hendrerit quam, mattis ultrices lorem tellus lacinia massa. Aliquam sit amet odio. Proin mauris. Integer dictum quam a quam accumsan lacinia. Pellentesque pulvinar feugiat eros. Suspendisse rhoncus. Sed consectetuer leo eu nisi. Suspendisse massa! Sed suscipit lacus sit amet elit! Aliquam sollicitudin condimentum turpis. Nunc ut augue! Maecenas eu eros. Morbi in urna consectetuer ipsum vehicula tristique.\n\n", font2));
+            ct.AddText(new Phrase("Donec imperdiet purus vel ligula. Vestibulum tempor, odio ut scelerisque eleifend, nulla sapien laoreet dui; vel aliquam arcu libero eu ante. Curabitur rutrum tristique mi. Sed lobortis iaculis arcu. Suspendisse mauris. Aliquam metus lacus, elementum quis, mollis non, consequat nec, tortor.\n", font2));
+            ct.AddText(new Phrase("Quisque id diam. Ut egestas leo a elit. Nulla in metus. Aliquam iaculis turpis non augue. Donec a nunc? Phasellus eu eros. Nam luctus. Duis eu mi. Ut mollis. Nulla facilisi. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Aenean pede. Nulla facilisi. Vestibulum mattis adipiscing nulla. Praesent orci ante, mattis in, cursus eget, posuere sed, mauris.\n\n", font2));
+            ct.AddText(new Phrase("Nulla facilisi. Nunc accumsan risus aliquet quam. Nam pellentesque! Aenean porttitor. Aenean congue ullamcorper velit. Phasellus suscipit placerat tellus. Vivamus diam odio, tempus quis, suscipit a, dictum eu; lectus. Sed vel nisl. Ut interdum urna eu nibh. Praesent vehicula, orci id venenatis ultrices, mauris urna mollis lacus, et blandit odio magna at enim. Pellentesque lorem felis, ultrices quis, gravida sed, pharetra vitae, quam. Mauris libero ipsum, pharetra a, faucibus aliquet, pellentesque in, mauris. Cras magna neque, interdum vel, varius nec; vulputate at, erat. Quisque vitae urna. Suspendisse potenti. Nulla luctus purus at turpis! Vestibulum vitae dui. Nullam odio.\n\n", font2));
+            ct.AddText(new Phrase("Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Sed eget mi at sem iaculis hendrerit. Nulla facilisi. Etiam sed elit. In viverra dapibus sapien. Aliquam nisi justo, ornare non, ultricies vitae, aliquam sit amet, risus! Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Phasellus risus. Vestibulum pretium augue non mi. Sed magna. In hac habitasse platea dictumst. Quisque massa. Etiam viverra diam pharetra ante. Phasellus fringilla velit ut odio! Nam nec nulla.\n\n", font2));
+            ct.AddText(new Phrase("Integer augue. Morbi orci. Sed quis nibh. Nullam ac magna id leo faucibus ornare. Vestibulum eget lectus sit amet nunc facilisis bibendum. Donec adipiscing convallis mi. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus enim. Mauris ligula lorem, pellentesque quis, semper sed, tristique sit amet, justo. Suspendisse potenti. Proin vitae enim. Morbi et nisi sit amet sapien ve.\n\n", font2));
+
+            float gutter = 15f;
+            float colwidth = (doc.Right - doc.Left - gutter) / 2;
+            float[] left = { doc.Left + 90f , doc.Top - 80f,
+                  doc.Left + 90f, doc.Top - 170f,
+                  doc.Left, doc.Top - 170f,
+                  doc.Left , doc.Bottom };
+
+            float[] right = { doc.Left + colwidth, doc.Top - 80f,
+                    doc.Left + colwidth, doc.Bottom };
+
+            float[] left2 = { doc.Right - colwidth, doc.Top - 80f,
+                    doc.Right - colwidth, doc.Bottom };
+
+            float[] right2 = {doc.Right, doc.Top - 80f,
+                    doc.Right, doc.Bottom };
+
+            int status = 0;
+            int i = 0;
+            //Checks the value of status to determine if there is more text
+            //If there is, status is 2, which is the value of NO_MORE_COLUMN
+            while (ColumnText.HasMoreText(status))
+            {
+                if (i == 0)
+                {
+                    //Writing the first column
+                    ct.SetColumns(left, right);
+                    i++;
+                }
+                else
+                {
+                    //write the second column
+                    ct.SetColumns(left2, right2);
+                }
+                //Needs to be here to prevent app from hanging
+                ct.YLine = doc.Top - 80f;
+                //Commit the content of the ColumnText to the document
+                //ColumnText.Go() returns NO_MORE_TEXT (1) and/or NO_MORE_COLUMN (2)
+                //In other words, it fills the column until it has either run out of column, or text, or both
+                status = ct.Go();
+            }
+        }
+        catch (Exception ex)
+        {
+            //Log(ex.Message);
+        }
+        finally
+        {
+            doc.Close();
+        }
+
+*/
+            #endregion
+
+            #region CUSTOM OFFSET FOR TEXT (WITH WORD WRAP)
+
+        /*      
+         
+        Document doc = new Document(PageSize.LETTER);
+        doc.SetMargins(0f, 0f, 0f, 0f);
+
+        MemoryStream memStream = new MemoryStream();
+
+        try
+        {
+            string path = Server.MapPath("PDFs");
+            string imagepath = Server.MapPath("images");
+
+            PdfWriter writer = PdfWriter.GetInstance(doc, new FileStream(path + "/SOAP.pdf", FileMode.Create));
+            doc.Open();
+
+            PdfContentByte cb = writer.DirectContent;
+            ColumnText ct = new ColumnText(cb);
+            Phrase myText = new Phrase("This is a text phrase");
+            string hi = "Hi";
+            Phrase myText2 = new Phrase("This is another test phrase");
+            Phrase myText3 = new Phrase(hi);
+
+            iTextSharp.text.Image img = iTextSharp.text.Image.GetInstance(imagepath + "/Test.jpg");
+            doc.Add(img);
+
+
+
+//parameters of SetSimpleColumn: (Phrase, leftmargin coordinate, bottommargin coordinate, box width, box height, line height, alignment)     
+            //NOTE: all dimensions start at the BOTTOM LEFT of the PDF... why? I dont fucking know, its retarded.
+            //NOTE: bottommargin determines x coordinate
+
+            ct.SetSimpleColumn(myText, 150, 100, 300, 700, 15, Element.ALIGN_LEFT);
+            ct.Go();
+
+            ct.SetSimpleColumn(myText2, 150, 100, 100, 317, 15, Element.ALIGN_LEFT);
+            ct.Go();
+
+            ct.SetSimpleColumn(myText3, 300, 100, 400, 317, 15, Element.ALIGN_LEFT);
+            ct.Go();
+
+        }
+
+        catch
+        {
+        }
+        finally
+        {
+        doc.Close();
+        }
+
+      */
+
+            #endregion
+
+
+
+
+
+
+        }
     }
 }
